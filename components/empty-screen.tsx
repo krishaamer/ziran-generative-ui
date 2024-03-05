136601ec4f0f47d1ae9c4ccbad95a953e8cb6f44
Kris Haamer <kris@haam.co>
2024-03-05 14:10:19 +0800

    Add default prompts based on survey questions:

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,6 +6,58 @@ const exampleMessages = [
     heading: "Check product sustainability",
     message: "What are some sustainable stocks?",
   },
+  {
+    heading: "買東西先查看產品的運輸距離（是不是當地食品）",
+    message: "買東西先查看產品的運輸距離（是不是當地食品）",
+  },
+  {
+    heading: "買東西先查看公司生產過程多環保",
+    message: "買東西先查看公司工人的員工福利多好",
+  },
+  {
+    heading: "買東西先了解哪些產品污染最嚴重，以便避免它們",
+    message: "買東西先了解哪些產品污染最嚴重，以便避免它們",
+  },
+  {
+    heading: "買東西先了解我吃的動物性食品，動物的生活環境",
+    message: "買東西先了解我吃的動物性食品，動物的生活環境",
+  },
+  {
+    heading: "買東西先尋找有機產品",
+    message: "買東西先尋找有機產品",
+  },
+  {
+    heading: "投資前查看AI摘要的消費者對公司環保的評論",
+    message: "投資前查看AI摘要的消費者對公司環保的評論",
+  },
+  {
+    heading: "投資前查看公司的環保認證和生態評分",
+    message: "投資前查看公司的環保認證和生態評分",
+  },
+  {
+    heading: "投資前比較公司的環保表現",
+    message: "投資前比較公司的環保表現",
+  },
+  {
+    heading: "用社交網絡認識其他環保的同學",
+    message: "用社交網絡認識其他環保的同學",
+  },
+  {
+    heading: "每個月查看我自己的環保分數報告（了解我用錢的方式多環保）",
+    message: "每個月查看我自己的環保分數報告（了解我用錢的方式多環保）",
+  },
+  {
+    heading: "如何讓我支持的公司更環保",
+    message: "如何讓我支持的公司更環保",
+  },
+  {
+    heading: "跟我的AI幫手討論環保問題",
+    message: "跟我的AI幫手討論環保問題",
+  },
+  {
+    heading: "老實說我對任何環保資訊都沒有太多興趣",
+    message: "老實說我對任何環保資訊都沒有太多興趣",
+  },
 ];
 
 export function EmptyScreen({
