0766018eefcb610b6563e3edd0db61541ec8b16f
Kris Haamer <kris@haam.co>
2024-03-14 13:22:23 +0800

    Switch to Google's official react map component

--- components/llm-map/index.tsx
+++ components/llm-map/index.tsx
@@ -1,45 +1,30 @@
 "use client";
-import React from "react";
-import GoogleMapReact from 'google-map-react';
 
-interface AnyReactComponentProps {
-  text: string;
-  lat: number;
-  lng: number;
-}
-
-const AnyReactComponent: React.FC<AnyReactComponentProps> = ({ text }) => <div>{text}</div>;
-
-interface DefaultProps {
-  center: {
-    lat: number;
-    lng: number;
-  };
-  zoom: number;
-}
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
 export default function SimpleMap() {
-  const defaultProps: DefaultProps = {
-    center: {
-      lat: 23.0124433,
-      lng: 120.212851
-    },
-    zoom: 13
-  };
+  const position = { lat: 23.0124433, lng: 120.212851 };
+  const marker1 = { lat: 23.0124433, lng: 120.212851 };
+  const marker2 = { lat: 23.018500, lng: 120.212751 };
+  const marker3 = { lat: 23.0124222, lng: 120.218951 };
 
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{
-          key:
-            process.env.REACT_APP_GOOGLE_MAPS_API_KEY ||
-            "AIzaSyB6uP_GJFF4Ngfxdp3aftquS7cYp_tgw18",
-        }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
+    <div
+      style={{ height: "500px", width: "100%" }}
+      className="rounded-md shadow-lg ring overflow-hidden"
+    >
+      <APIProvider
+        apiKey={
+          process.env.REACT_APP_GOOGLE_MAPS_API_KEY ||
+          "AIzaSyB6uP_GJFF4Ngfxdp3aftquS7cYp_tgw18"
+        }
       >
-        <AnyReactComponent lat={23.123411} lng={120.337844} text="My Marker" />
-      </GoogleMapReact>
+        <Map defaultCenter={position} defaultZoom={13}>
+          <Marker position={marker1} />
+          <Marker position={marker2} />
+          <Marker position={marker3} />
+        </Map>
+      </APIProvider>
     </div>
   );
 }
