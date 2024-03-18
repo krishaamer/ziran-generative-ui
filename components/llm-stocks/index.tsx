6e85234f88883200f2d9d9eb936c733a80557f88
Kris Haamer <kris@haam.co>
2024-03-18 12:32:28 +0800

    Separate User and System message styles

--- components/llm-stocks/index.tsx
+++ components/llm-stocks/index.tsx
@@ -5,7 +5,7 @@ import { StockSkeleton } from './stock-skeleton';
 import { StocksSkeleton } from './stocks-skeleton';
 
 export { spinner } from './spinner';
-export { BotCard, BotMessage, SystemMessage } from './message';
+export { BotCard, BotMessage, SystemMessage } from '../shared/message';
 
 const Stock = dynamic(() => import('./stock').then(mod => mod.Stock), {
   ssr: false,
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,9 +1,24 @@
-export function Sidebar() {
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
+export function Sidebar() {
   return (
-    <div className="relative mx-auto max-w-2xl px-4">
-     <h2>I'm a consumer of Brand. Should I invest?</h2>
-     <p>This brand is produced in factories A, B, C. Sust score D. Compare to Brand YXZ.</p>
+    <div className="relative mx-auto max-w-2xl px-4 mt-10">
+      <Card>
+          <CardHeader>
+            <CardTitle><h2>I'm a consumer of Brand. Should I invest?</h2></CardTitle>
+          </CardHeader>
+          <CardContent>
+            This brand is produced in factories A, B, C. Sust score D. Compare
+            to Brand YXZ.
+        </CardContent>
+      </Card>
     </div>
   );
 }
