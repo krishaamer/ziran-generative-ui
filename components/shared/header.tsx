539350e50d0f09cf33351d769793e649afc0da65
Kris Haamer <kris@haam.co>
2024-04-10 20:41:37 +0800

    Add dark mode button

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,11 +1,15 @@
-'use client';
+"use client";
+
+import ThemeSwitcher from "@/components/shared/theme";
 
 export default function Header() {
   return (
     <div className="sticky top-0 z-50 flex items-center justify-between w-full px-4 border-b h-8 shrink-0 bg-background uppercase">
       <div />
       <a href="/">綠濾</a>
-      <div />
+      <div>
+        <ThemeSwitcher />
+      </div>
     </div>
   );
 }
