ce620a5e3be97c306f1b688c8dc3ee8daeb8bcb6
Kris Haamer <kris@haam.co>
2024-04-11 03:10:52 +0800

    Nicer button

--- components/search.tsx
+++ components/search.tsx
@@ -88,6 +88,7 @@ export function Search({
                   <TooltipTrigger asChild>
                     <Button
                       type="submit"
+                      className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                       size="lg"
                       disabled={inputValue === ""}
                     >
