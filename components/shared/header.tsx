82a7205e064efe8fe4e74be06cafe6c07827c673
Kris Haamer <kris@haam.co>
2024-04-11 16:09:49 +0800

    Try to add stock charts

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -5,8 +5,8 @@ import ThemeSwitcher from "@/components/shared/theme";
 export default function Header() {
   return (
     <div className="sticky top-0 z-50 flex items-center justify-between w-full px-4 border-b h-8 shrink-0 bg-background uppercase">
-      <div />
       <a href="/">綠濾</a>
+      <a href="/">怎麼知道自己的錢花去支持了啥物呢？</a>
       <div>
         <ThemeSwitcher />
       </div>
