ab71182f606d78dc5ada6e41dcbdab8f4d0231be
Kris Haamer <kris@haam.co>
2024-04-12 23:22:55 +0800

    Add tooltips

--- components/search.tsx
+++ components/search.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
@@ -17,8 +15,8 @@ export function Search({
   submitMessage: (message: string) => void;
 }) {
   const inputRef = useRef<HTMLTextAreaElement>(null);
-
   const [inputValue, setInputValue] = useState("");
+  const [isFocused, setIsFocused] = useState(false); // State to track focus
   const { formRef, onKeyDown } = useEnterSubmit();
 
   useEffect(() => {
@@ -46,61 +44,62 @@ export function Search({
   }, [inputRef]);
 
   return (
-    <div className="relative inset-x-0 bottom-0 w-full">
-      <div className="mx-auto sm:max-w-2xl sm:px-4">
-        <div>
-          <form
-            ref={formRef}
-            onSubmit={async (e: any) => {
-              e.preventDefault();
-
-              // Blur focus on mobile
-              if (window.innerWidth < 600) {
-                e.target["message"]?.blur();
-              }
-
-              const value = inputValue.trim();
-              setInputValue("");
-              if (!value) return;
-
-              // Add user message UI
-              submitMessage(value);
-            }}
-          >
-            <div className="relative flex flex-col w-full px-4 overflow-hidden max-h-60 grow shadow-sm bg-background rounded-xl border">
-              <Textarea
-                ref={inputRef}
-                tabIndex={0}
-                onKeyDown={onKeyDown}
-                placeholder="我的問題～～"
-                className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
-                autoFocus
-                spellCheck={false}
-                autoComplete="off"
-                autoCorrect="off"
-                name="message"
-                rows={1}
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-              />
-              <div className="absolute right-2 top-2 sm:right-2">
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Button
-                      type="submit"
-                      className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                      size="lg"
-                      disabled={inputValue === ""}
-                    >
-                      <span>問綠濾</span>
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent>問綠濾</TooltipContent>
-                </Tooltip>
+    <div
+      className={`sticky bottom-0 z-50 flex items-center justify-between w-full px-4 border-b h-20 shrink-0 ${isFocused ? "bg-pink-50" : "bg-background"} backdrop-blur-xl docu-shadow`}
+    >
+      <div className="relative inset-x-0 bottom-0 w-full">
+        <div className="mx-auto sm:max-w-2xl sm:px-4">
+          <div>
+            <form
+              ref={formRef}
+              onSubmit={async (e: any) => {
+                e.preventDefault();
+                if (window.innerWidth < 600) {
+                  e.target["message"]?.blur();
+                }
+                const value = inputValue.trim();
+                setInputValue("");
+                if (!value) return;
+                submitMessage(value);
+              }}
+            >
+              <div className="relative flex flex-col w-full px-4 overflow-hidden max-h-60 grow shadow-sm bg-background rounded-xl border">
+                <Textarea
+                  ref={inputRef}
+                  tabIndex={0}
+                  onKeyDown={onKeyDown}
+                  onFocus={() => setIsFocused(true)} // Update focus state to true
+                  onBlur={() => setIsFocused(false)} // Update focus state to false
+                  placeholder="我的問題～～"
+                  className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
+                  autoFocus
+                  spellCheck={false}
+                  autoComplete="off"
+                  autoCorrect="off"
+                  name="message"
+                  rows={1}
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                />
+                <div className="absolute right-2 top-2 sm:right-2">
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button
+                        type="submit"
+                        className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                        size="lg"
+                        disabled={inputValue === ""}
+                      >
+                        <span>問綠濾</span>
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>問綠濾</TooltipContent>
+                  </Tooltip>
+                </div>
               </div>
-            </div>
-          </form>
-          <FooterText className="hidden sm:block" />
+            </form>
+            <FooterText className="hidden sm:block" />
+          </div>
         </div>
       </div>
     </div>
