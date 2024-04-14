0ea73e71615961ba85a5ee12411eeb7bb67f805a
Kris Haamer <kris@haam.co>
2024-04-14 21:26:41 +0800

    Code style

--- components/llm-stocks/index.tsx
+++ components/llm-stocks/index.tsx
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
-import dynamic from 'next/dynamic';
-import { StockSkeleton } from './stock-skeleton';
-import { StocksSkeleton } from './stocks-skeleton';
+import dynamic from "next/dynamic";
+import { StockSkeleton } from "./stock-skeleton";
+import { StocksSkeleton } from "./stocks-skeleton";
 
-export { spinner } from './spinner';
-export { BotCard, BotMessage, SystemMessage } from '../shared/message';
+export { spinner } from "./spinner";
+export { BotCard, BotMessage, SystemMessage } from "../shared/message";
 
-const Stock = dynamic(() => import('./stock').then(mod => mod.Stock), {
+const Stock = dynamic(() => import("./stock").then((mod) => mod.Stock), {
   ssr: false,
   loading: () => <StockSkeleton />,
 });
 
-const Stocks = dynamic(() => import('./stocks').then(mod => mod.Stocks), {
+const Stocks = dynamic(() => import("./stocks").then((mod) => mod.Stocks), {
   ssr: false,
   loading: () => <StocksSkeleton />,
 });
--- components/llm-stocks/stocks.tsx
+++ components/llm-stocks/stocks.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { useActions, useUIState } from 'ai/rsc';
+import { useActions, useUIState } from "ai/rsc";
 
-import type { AI } from '../../app/action';
+import type { AI } from "../../app/action";
 
 export function Stocks({ stocks }: { stocks: any[] }) {
   const [, setMessages] = useUIState<typeof AI>();
@@ -10,21 +10,21 @@ export function Stocks({ stocks }: { stocks: any[] }) {
 
   return (
     <div className="flex flex-col sm:flex-row text-sm gap-2 mb-4 overflow-y-scroll pb-4">
-      {stocks.map(stock => (
+      {stocks.map((stock) => (
         <button
           key={stock.symbol}
           className="bg-zinc-900 text-left p-2 rounded-lg flex flex-row gap-2 cursor-pointer hover:bg-zinc-800 sm:w-52"
           onClick={async () => {
             const response = await submitUserMessage(`View ${stock.symbol}`);
-            setMessages(currentMessages => [...currentMessages, response]);
+            setMessages((currentMessages) => [...currentMessages, response]);
           }}
         >
           <div
             className={`text-xl ${
-              stock.delta > 0 ? 'text-green-600' : 'text-red-600'
+              stock.delta > 0 ? "text-green-600" : "text-red-600"
             } p-2 w-11 bg-white/10 flex flex-row justify-center rounded-md`}
           >
-            {stock.delta > 0 ? '↑' : '↓'}
+            {stock.delta > 0 ? "↑" : "↓"}
           </div>
           <div className="flex flex-col">
             <div className="text-zinc-300 bold uppercase">{stock.symbol}</div>
@@ -33,14 +33,14 @@ export function Stocks({ stocks }: { stocks: any[] }) {
           <div className="flex flex-col ml-auto">
             <div
               className={`${
-                stock.delta > 0 ? 'text-green-600' : 'text-red-600'
+                stock.delta > 0 ? "text-green-600" : "text-red-600"
               } bold uppercase text-right`}
             >
               {` ${((stock.delta / stock.price) * 100).toFixed(2)}%`}
             </div>
             <div
               className={`${
-                stock.delta > 0 ? 'text-green-700' : 'text-red-700'
+                stock.delta > 0 ? "text-green-700" : "text-red-700"
               } text-base text-right`}
             >
               {stock.delta}
