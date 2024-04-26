964564328116332689cadfb32702a2d414db9d15
Kris Haamer <kris@haam.co>
2024-04-26 13:31:15 +0800

    Try to load analytics only on client side using separate component

---
+++ components/shared/analytics.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+import { hotjar } from "react-hotjar";
+import { init as initFullStory, FullStory } from "@fullstory/browser";
+
+export default function ClientAnalytics() {
+
+    useEffect(() => {
+
+      initFullStory({ orgId: "o-1XVGW4-na1" });
+
+      FullStory("trackEvent", {
+        name: "Ziran Init",
+        properties: {
+          myprop: "somedata",
+        },
+      });
+
+      hotjar.initialize({ id: 4956812, sv: 6 });
+    }, []);
+
+  return (
+    <></>
+  );
+}
