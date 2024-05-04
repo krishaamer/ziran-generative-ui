c0a458694a0cc8d99a32297e103eec6b56eff6f7
Kris Haamer <kris@haam.co>
2024-05-04 19:04:20 +0800

    Add API to get origin

---
+++ app/api/origin/route.ts
@@ -0,0 +1,28 @@
+import "server-only";
+import { kv } from "@vercel/kv";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const reportID = searchParams.get("id");
+
+  if (typeof reportID !== "string") {
+    return NextResponse.json(
+      { error: "reportID is required and must be a string" },
+      { status: 400 }
+    );
+  }
+
+  const key = reportID + "_origin";
+  const originData = await kv.get(key);
+
+  return new Response(
+    JSON.stringify({ origin: originData || "No data found" }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+}
