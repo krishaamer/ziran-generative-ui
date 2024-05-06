3ac9ad442bac761c54e389c5d48c1e533f7af6e6
Kris Haamer <kris@haam.co>
2024-05-06 14:49:05 +0800

    Conditional UI if report is available

--- components/origin.tsx
+++ components/origin.tsx
@@ -1,38 +1,44 @@
-"use client";
-
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { getDomainFromURL } from "@/lib/utils";
 
 export default function Origin() {
-  const [origin, setOrigin] = useState({ url: ""});
+  const [origin, setOrigin] = useState({ url: "" });
+  const [reportID, setReportID] = useState<string | null>(null);
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    const reportID = searchParams.get("id");
+    const id = searchParams.get("id");
+    setReportID(id);
 
-    if (reportID) {
-      fetch(`/api/origin?id=${reportID}`)
+    if (id) {
+      fetch(`/api/origin?id=${id}`)
         .then((response) => response.json())
         .then((data) => {
-          setOrigin({url: data?.origin});
+          setOrigin({ url: data?.origin });
         })
         .catch((error) => {
-          console.error("Failed to origin data", error);
+          console.error("Failed to fetch origin data", error);
         });
     }
   }, [searchParams]);
 
   return (
     <div>
-      {origin ? (
-        <span>
-          資料來源：
-          <a href={origin?.url} target="_blank" className="link font-bold">{getDomainFromURL(origin?.url)}</a>
-        </span>
-      ) : (
-        ""
-      )}
+      {reportID &&
+        origin.url && (
+          <span>
+            資料來源：
+            <a
+              href={origin.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link font-bold"
+            >
+              {getDomainFromURL(origin.url)}
+            </a>
+          </span>
+        )}
     </div>
   );
 }
