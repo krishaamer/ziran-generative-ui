0faa2983b8850c296d3c19b9b7ced1228c38631c
Kris Haamer <kris@haam.co>
2024-07-29 13:18:35 +0300

    Change copy

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
   return (
     <div className="sticky top-0 z-50 flex items-center justify-between w-full px-4 border-b h-8 shrink-0 text-white bg-gradient-to-br from-pink-500 to-orange-400 font-medium text-sm text-center">
       <a href="/">綠濾助手</a>
-      <a href="/">怎麼知道自己的錢花去支持了啥物呢？</a>
+      <a href="/">錢花去支持了啥物呢？</a>
       <div className="flex space-x-4">
         <div>
           {session?.data?.user?.name ? (
