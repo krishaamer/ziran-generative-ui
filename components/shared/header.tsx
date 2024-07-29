f85b7c82d07f1e348f94bb6b0a6a61fd66da4d93
Kris Haamer <kris@haam.co>
2024-07-29 11:55:21 +0300

    Add login to header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -2,18 +2,22 @@
 
 import ThemeSwitcher from "@/components/shared/theme";
 import { useSession } from "next-auth/react";
+import SignIn from "../llm-login";
 
 export default function Header() {
-
   const session = useSession();
- 
+
   return (
     <div className="sticky top-0 z-50 flex items-center justify-between w-full px-4 border-b h-8 shrink-0 text-white bg-gradient-to-br from-pink-500 to-orange-400 font-medium text-sm text-center">
       <a href="/">綠濾助手</a>
       <a href="/">怎麼知道自己的錢花去支持了啥物呢？</a>
       <div className="flex space-x-4">
         <div>
-          <p>{session?.data?.user?.name ? `Welcome ${session?.data?.user?.name}` : ""}</p>
+          {session?.data?.user?.name ? (
+            <p>`歡迎 ${session?.data?.user?.name}`</p>
+          ) : (
+            <SignIn />
+          )}
         </div>
         <ThemeSwitcher />
       </div>
