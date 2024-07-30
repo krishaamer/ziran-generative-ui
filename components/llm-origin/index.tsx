186771d4047941ba1b519e1260dff532fdb39039
Kris Haamer <kris@haam.co>
2024-07-30 20:59:14 +0300

    Add origin map

--- components/llm-origin/index.tsx
+++ components/llm-origin/index.tsx
@@ -1,22 +1,50 @@
 "use client";
 
+import React, { useState } from "react";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { BotMessage } from "@/components/shared/message";
 import { useActions, useUIState } from "ai/rsc";
 import { Button } from "@/components/ui/button";
+import { Polyline } from "@/components/polyline";
 import type { AI } from "../../app/action";
 
 export default function Origin() {
-
+  const position = { lat: 41, lng: 73 };
+  const [flightPlanCoordinates, setFlightPlanCoordinates] = useState([
+    { lat: 23.6978, lng: 120.9605 }, // Taiwan
+    { lat: 58.5953, lng: 25.0136 }, // Estonia
+  ]);
   const [, setMessages] = useUIState<typeof AI>();
   const { submitUserMessage } = useActions();
- 
+
   return (
     <>
-      <div
-        style={{ height: "500px", width: "100%" }}
-        className="rounded-lg shadow-lg ring overflow-hidden"
+      <BotMessage>產品來源</BotMessage>
+      <APIProvider
+        apiKey={
+          process.env.REACT_APP_GOOGLE_MAPS_API_KEY ||
+          "AIzaSyB6uP_GJFF4Ngfxdp3aftquS7cYp_tgw18"
+        }
       >
-        <h1 className="text-3xl">產品來源</h1>
-      </div>
+        <div
+          style={{ height: "500px", width: "100%" }}
+          className="rounded-lg shadow-lg ring overflow-hidden"
+        >
+          <Map
+            zoom={3}
+            center={position}
+            gestureHandling={"cooperative"}
+            disableDefaultUI={true}
+          >
+            <Polyline
+              path={flightPlanCoordinates}
+              strokeColor="#FF0000"
+              strokeOpacity={1.0}
+              strokeWeight={2}
+            />
+          </Map>
+        </div>
+      </APIProvider>
       <Button
         variant="outline"
         size="lg"
